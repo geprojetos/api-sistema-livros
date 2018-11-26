85d0184bd1df2ba80441dd31aa8eba3cae794942
Genilson <geprojetosbrasil@gmail.com>
2018-11-26 14:06:52 -0200

    Criando comandos usados pelo mongo e instalando o mongoose

--- config/express.js
+++ config/express.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var app     = express();
-var consign = require('consign');
+var express     = require('express');
+var app         = express();
+var consign     = require('consign');
+var bodyparser  = require('body-parser');
 
 app.use(express.static('./public'));
+app.use(bodyparser.json());
 
 consign({ cwd: 'app' })
     .include('controllers')
