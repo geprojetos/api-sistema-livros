25e1ea10243473bdf53c492491730bcb9090856f
Genilson <geprojetosbrasil@gmail.com>
2018-11-26 18:07:45 -0200

    Padronizando mensagens de erro na api

--- app/controllers/autores.js
+++ app/controllers/autores.js
@@ -13,7 +13,10 @@ api.listagem = function(req, res) {
         }, erro => {
 
             console.log(erro);
-            res.status(500).json(erro)
+            res.json({
+                status: 'Não foi possível listar os autores cadastrados',
+                message: erro.message,
+            })
         })
 };
 
@@ -22,11 +25,16 @@ api.cadastrar = function(req, res) {
     autores
         .create(req.body)
         .then( autor => {
+
             console.log('Autor cadastrado com sucesso ' + autor);
             res.json(autor)
         }, erro => {
+            
             console.log(erro);
-            res.status(500).json(erro)
+            res.json({
+                status: 'Não foi possivel cadastrar o autor ' + autor,
+                message: erro.message,
+            })
         });
     
 };
@@ -40,7 +48,7 @@ api.buscar = function(req, res) {
             console.log('Autor encontrado com sucesso ' + autor);
             res.json(autor);
         }, erro => {
-            
+
             console.log('Autor não encontrado ' + req.params.id);
             res.json({
                 status: 'Não foi possivel encontrar o autor ' + req.params.id,
