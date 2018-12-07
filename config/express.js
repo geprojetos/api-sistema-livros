55440b17954d066ce9cfdf363b23d32c106dd018
Genilson <geprojetosbrasil@gmail.com>
2018-12-07 20:01:38 -0200

    Comentando script caso deseje utilizar api em um domínio separado da aplicaçã

--- config/express.js
+++ config/express.js
@@ -4,6 +4,12 @@ var consign     = require('consign');
 var bodyparser  = require('body-parser');
 
 app.use(express.static('./public'));
+// app.use(function(req, res, next) {
+// 	res.header("Access-Control-Allow-Origin", "*");
+// 	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+// 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// 	next();
+// });
 app.use(bodyparser.json());
 
 consign({ cwd: 'app' })
